1079cc8916f8ed45fa65770f8b6623970cdbb154
guilherm5 <estudosrodriguesti@gmail.com>
2023-02-19 10:24:02 -0300

    upando create, read, delete e update de postagems

--- main.js
+++ main.js
@@ -1,41 +1,93 @@
 //abrindo servidor e definindo porta 
 const express = require('express');
 const app = express();
-
-//----------------------------------------------//
-//definindo conexão com mysql 
-const Sequelize = require ('sequelize')
-const sequelize = new Sequelize('projeto_nodejs', 'guilherme', '123', {
-    host: "localhost",
-    dialect: 'mysql'
-})
-sequelize.authenticate().then(function(){
-    console.log("Conectado com sucesso")
-}).catch(function(erro){
-    console.log("Erro ao realiar conexão")
-})
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
+const Post = require('./models/Post');
+const { where } = require('sequelize');
 
 //----------------------------------------------//
 //definindo tamplate engine
-const handlebars = require('express-handlebars');
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+
+app.engine('handlebars', handlebars.engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+
+        allowProtoMethodsByDefault: true,
+    }
+}));
 app.set('view engine', 'handlebars');
 
 
 //----------------------------------------------//
 //configurando body parser
-const bodyParser = require('body-parser')
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 //criando rotas 
+
+app.get('/', function(req,res){
+    Post.findAll({order:[['id', 'DESC']]}).then(function(posts){
+        res.render('layouts/home', {posts: posts})
+    })
+   
+})
+
 app.get('/cad', function (req, res) {
     res.render('layouts/formulario');
 });
 
 app.post('/add', function (req, res) {
-    res.send(`Texto: ${req.body.titulo} Conteudo: ${req.body.conteudo}`)
+
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(function(){
+        res.redirect('/')
+    }).catch(function(erro){
+        res.send("Erro ao criar post: " + erro)
+    })
+
   });
+
+app.get('/deletar/:id', function(req, res){
+    Post.destroy({where: {'id': req.params.id}}).then(function(){
+        res.send("Postagem deletada com sucesso")
+    }).catch(function(erro){
+        res.send("Essa postagem nao existe")
+    })
+})
+
+
+
+app.get('/edit/:id', function(req, res){
+  Post.findByPk(req.params.id)
+    .then(post => {
+      res.render('layouts/form-edit', {
+        id: req.params.id,
+        titulo: post.titulo,
+        conteudo: post.conteudo
+      })
+    })
+    .catch(err => {
+      res.send('Post não encontrado!')
+    })
+})
+app.post('/editado/:id', function(req, res){
+  Post.update({
+    titulo: req.body.titulo,
+    conteudo: req.body.conteudo
+  },
+  {
+    where: { id: req.params.id }
+  }).then(function(){
+    res.redirect('/')
+  }).catch(function(err){
+    console.log(err);
+  })
+})
 
 //rodando servidor
 app.listen(3000, function () {
